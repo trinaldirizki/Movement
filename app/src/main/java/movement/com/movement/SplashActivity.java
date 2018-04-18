bfa130bfe073c523e9c8740251adebf0bafccc17
trinaldirizki <trinaldirizki@gmail.com>
2018-04-18 17:38:05 +0300

    Added Splash screen into the app.

--- app/src/main/java/movement/com/movement/SplashActivity.java
+++ app/src/main/java/movement/com/movement/SplashActivity.java
@@ -1,7 +1,11 @@
 package movement.com.movement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ImageView imageView = findViewById(R.id.imageSplash);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+        imageView.startAnimation(animation);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        thread.start();
     }
 }

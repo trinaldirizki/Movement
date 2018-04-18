c88f06dc3bb5c73d953ddd0cf70d15ddc9380ed9
trinaldirizki <trinaldirizki@gmail.com>
2018-04-18 18:32:34 +0300

    Added Login screen into the app. Created rounded button.

--- app/src/main/java/movement/com/movement/SplashActivity.java
+++ app/src/main/java/movement/com/movement/SplashActivity.java
@@ -24,7 +24,7 @@ public class SplashActivity extends AppCompatActivity {
 
                 try {
                     sleep(3000);
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                     startActivity(intent);
                     finish();
                 } catch (InterruptedException e) {

0a47385971e1fa5b05ed7c421a3d0d544ed3644b
trinaldirizki <trinaldirizki@gmail.com>
2018-04-20 03:28:55 +0300

    Improved UI design for MainActivity layout.

--- app/src/main/java/movement/com/movement/LoginActivity.java
+++ app/src/main/java/movement/com/movement/LoginActivity.java
@@ -49,12 +49,10 @@ public class LoginActivity extends AppCompatActivity {
 
     CallbackManager mCallbackManager;
     Button mFacebookButton;
-    Button mGoogleButton;
-    SignInButton mSignInButton;
     private GoogleSignInClient mGoogleSignInClient;
     private FirebaseAuth mAuth;
     private static final String TAG = LoginActivity.class.getSimpleName();
-    private static final int RC_SIGN_IN = 9001;
+    private static final int RC_GOOGLE = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,8 +60,6 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         mAuth = FirebaseAuth.getInstance();
-        mSignInButton = findViewById(R.id.sign_in_button);
-        mSignInButton.setSize(SignInButton.SIZE_STANDARD);
         mFacebookButton = findViewById(R.id.buttonLoginFacebook);
         mCallbackManager = CallbackManager.Factory.create();
         LoginManager.getInstance().registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
@@ -84,8 +80,6 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
@@ -95,9 +89,8 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        // updateUI(currentUser);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
     }
 
     private void handleFacebookAccessToken(AccessToken accessToken) {
@@ -109,16 +102,14 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            // updateUI(user);
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
                         } else {
-                            // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithCredential:failure", task.getException());
                             Toast.makeText(LoginActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
-                            // updateUI(null);
                         }
 
                     }
@@ -128,47 +119,22 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
+        if (requestCode == RC_GOOGLE) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
+        } else {
+            mCallbackManager.onActivityResult(requestCode, resultCode, data);
         }
-        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
             Log.d(TAG, account.getDisplayName() + " " + account.getEmail() + " " + account.getFamilyName());
-            // Signed in successfully, show authenticated UI.
-            // updateUI(account);
+            launchMainActivity();
         } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
             Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-            // updateUI(null);
         }
-    }
-
-    private void displayUserInfo(JSONObject object) {
-        String first_name, last_name, email, id;
-        first_name = "";
-        last_name = "";
-        email = "";
-        id = "";
-
-        try {
-            first_name = object.getString("first_name");
-            last_name = object.getString("last_name");
-            email = object.getString("email");
-            id = object.getString("id");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        Log.i("Login Info", first_name + " " + last_name + " " + email + " " + id);
     }
 
     public void loginFacebook(View view) {
@@ -176,11 +142,12 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void loginGoogle(View view) {
-        signIn();
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_GOOGLE);
     }
 
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
+    private void launchMainActivity(){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/movement/com/movement/MainActivity.java
+++ app/src/main/java/movement/com/movement/MainActivity.java
@@ -1,13 +1,44 @@
 package movement.com.movement;
 
+import android.preference.PreferenceManager;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mStartActivity;
+    ImageView mImageWalking, mImageRunning, mImageCycling, mImageChecked;
+    ConstraintLayout mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLayout = findViewById(R.id.layoutMain);
+        mImageWalking = findViewById(R.id.imageWalking);
+        mImageRunning = findViewById(R.id.imageRunning);
+        mImageCycling = findViewById(R.id.imageCycling);
+        mImageChecked = findViewById(R.id.imageChecked);
+        mStartActivity = findViewById(R.id.buttonStartActivity);
     }
+
+    public void selectActivity(View view){
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(mLayout);
+
+        constraintSet.connect(mImageChecked.getId(), ConstraintSet.BOTTOM, view.getId(), ConstraintSet.BOTTOM);
+        constraintSet.connect(mImageChecked.getId(), ConstraintSet.END, view.getId(), ConstraintSet.END);
+        constraintSet.applyTo(mLayout);
+    }
+
+    private void checkActivity(View view){
+
+    }
+
 }

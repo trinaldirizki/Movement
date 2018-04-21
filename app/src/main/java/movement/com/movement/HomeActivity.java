710319f38c0a783b3b14c358f134441e408c7e04
trinaldirizki <trinaldirizki@gmail.com>
2018-04-21 16:07:10 +0300

    Removed left Navigation Drawer.

--- app/src/main/java/movement/com/movement/HomeActivity.java
+++ app/src/main/java/movement/com/movement/HomeActivity.java
@@ -2,6 +2,8 @@ package movement.com.movement;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -13,9 +15,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    Button mStartActivity;
+    ImageView mImageWalking, mImageRunning, mImageCycling, mImageChecked;
+    ConstraintLayout mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,22 +34,21 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view_right);
         navigationView.setNavigationItemSelectedListener(this);
+
+        mLayout = findViewById(R.id.layoutMain);
+        mImageWalking = findViewById(R.id.imageWalking);
+        mImageRunning = findViewById(R.id.imageRunning);
+        mImageCycling = findViewById(R.id.imageCycling);
+        mImageChecked = findViewById(R.id.imageChecked);
+        mStartActivity = findViewById(R.id.buttonStartActivity);
     }
 
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else if (drawer.isDrawerOpen(GravityCompat.END)) {
+        if (drawer.isDrawerOpen(GravityCompat.END)) {
             drawer.closeDrawer(GravityCompat.END);
         } else {
             super.onBackPressed();
@@ -69,26 +78,55 @@ public class HomeActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        String text = "";
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                text = getString(R.string.home);
+                break;
+            case R.id.nav_profile:
+                text = getString(R.string.profile);
+                break;
+            case R.id.nav_leader_board:
+                text = getString(R.string.leader_board);
+                break;
+            case R.id.nav_news:
+                text = getString(R.string.news_feed);
+                break;
+            case R.id.nav_help:
+                text = getString(R.string.help_center);
+                break;
+            case R.id.nav_share:
+                text = getString(R.string.share);
+                break;
+            case R.id.nav_settings:
+                text = getString(R.string.settings);
+                break;
+            case R.id.nav_sign_out:
+                text = getString(R.string.sign_out);
+                break;
         }
 
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        drawer.closeDrawer(GravityCompat.END);
         return true;
     }
 
+    public void selectActivity(View view){
+        String selected = "";
+        if (view == mImageWalking){
+            selected = "walking";
+        } else if (view == mImageRunning){
+            selected = "running";
+        } else {
+            selected = "cycling";
+        }
+
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(mLayout);
+
+        constraintSet.connect(mImageChecked.getId(), ConstraintSet.BOTTOM, view.getId(), ConstraintSet.BOTTOM);
+        constraintSet.connect(mImageChecked.getId(), ConstraintSet.END, view.getId(), ConstraintSet.END);
+        constraintSet.applyTo(mLayout);
+    }
 }

ccaf80322bc1917fddc1151c448c457961844e49
trinaldirizki <trinaldirizki@gmail.com>
2018-06-10 01:04:25 +0300

    Improved design on Progress Activity.

--- app/src/main/java/movement/com/movement/OverviewActivity.java
+++ app/src/main/java/movement/com/movement/OverviewActivity.java
@@ -1,7 +1,9 @@
 package movement.com.movement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class OverviewActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class OverviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
+    }
+
+    public void startMoving(View view) {
+        Intent intent = new Intent(this, ProgressActivity.class);
+        startActivity(intent);
     }
 }

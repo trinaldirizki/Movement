e49ed211ebfec80916c00fe2f1a9d997f998fcb0
Trinaldi Rizki <trinaldirizki@gmail.com>
2018-06-11 20:04:35 +0300

    Made changes to News activity.

--- app/src/main/java/movement/com/movement/NewsActivity.java
+++ app/src/main/java/movement/com/movement/NewsActivity.java
@@ -2,6 +2,13 @@ package movement.com.movement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+import movement.com.movement.adapter.NewsAdapter;
+import movement.com.movement.model.News;
 
 public class NewsActivity extends AppCompatActivity {
 
@@ -9,5 +16,15 @@ public class NewsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        initRecylerNews();
+    }
+
+    private void initRecylerNews() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        RecyclerView recyclerNews = findViewById(R.id.recycler_news);
+        recyclerNews.setLayoutManager(layoutManager);
+        NewsAdapter adapter = new NewsAdapter(this, new List<News>());
+        recyclerNews.setAdapter(adapter);
     }
 }
